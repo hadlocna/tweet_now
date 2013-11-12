faac6d87f0d59818500430c5218a96af1fdaee35
Nathan Hadlock <hadlocna@gmail.com>
2013-11-12 12:54:39 -0600

    jquery status updates added

--- public/js/application.js
+++ public/js/application.js
@@ -5,9 +5,12 @@ $(document).ready(function() {
     var status = $('<h4 id = "processing"> The form is currently processing </h4>');
     $('#welcome').append(status);
     var tweet_text = { "tweet_text": $('#tweet_text').val()};
-      alert(tweet_text);
+
     $.post('/tweet', tweet_text, function(data){
-      alert("Data Loaded: " + data)
+      $('#processing').hide();
+      var success = $('<h4 id = "success"> You successfully posted: ' + data + '</h4>');
+      $('#welcome').append(success)
+
     },"json");
 
   });
